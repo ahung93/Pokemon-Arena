51c545de32202d54a2e0322a743590338bab3b5b
Alvin Hung <ahung93@gmail.com>
2015-06-03 21:13:26 -0700

    Basic folder setup and stuff

--- Pokemon-Arena/Assets/Scripts/Pokemon.cs
+++ Pokemon-Arena/Assets/Scripts/Pokemon.cs
@@ -1,6 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pokemon {
-	private enum NonVolatileStatus { Burn, Freeze, Paralyze, Sleep, Poison, Toxic };
+namespace CollectionsApplications
+{
+	abstract class Pokemon {
+
+		// May need to modify to include other status
+		private enum NonVolatileStatus { Burn, Freeze, Paralyze, Sleep, Poison, Toxic };
+
+		// Implement IVs
+		private Hashtable ivs;
+
+		// Implement EVs
+		private Hashtable evs;
+
+		// Implement Status Buffs/Debuffs
+
+		private Hashtable stats;
+		// Figure out function pointers for Move 1
+		// 									Move 2
+		//									Move 3
+		//									Move 4
+	}
 }
