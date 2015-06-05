7ae74c45393ad759fbd5492cd52598412125002d
Alvin Hung <ahung93@gmail.com>
2015-06-04 22:35:21 -0700

    More resource folders and class update

--- Pokemon-Arena/Assets/Scripts/Pokemon.cs
+++ Pokemon-Arena/Assets/Scripts/Pokemon.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 namespace CollectionsApplications
 {
-	abstract class Pokemon {
+	class Pokemon {
 
+		private string species;
 		// May need to modify to include other status
-		private enum NonVolatileStatus { Burn, Freeze, Paralyze, Sleep, Poison, Toxic };
+		private enum NonVolatileStatus { Healthy, Burn, Freeze, Paralyze, Sleep, Poison, Toxic, Fainted };
+
+		private enum Gender { Male, Female, None };
 
 		// Implement IVs
 		private Hashtable ivs;
@@ -17,9 +20,21 @@ namespace CollectionsApplications
 		// Implement Status Buffs/Debuffs
 
 		private Hashtable stats;
-		// Figure out function pointers for Move 1
-		// 									Move 2
-		//									Move 3
-		//									Move 4
+
+		// Figure out delegates for Move 1
+		// 							Move 2
+		//							Move 3
+		//							Move 4
+		// Make sure that it is the SAME object passed in (or a reference), cannot be a duplicate object
+		delegate void fptrMove(Pokemon pokemon);
+
+		private fptrMove move1;
+		private fptrMove move2;
+		private fptrMove move3;
+		private fptrMove move4;
+
+		public Pokemon() {
+			// Figure out how to parse JSON data 
+		}
 	}
 }
